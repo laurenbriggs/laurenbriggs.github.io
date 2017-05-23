f8a956d29cf93d91905800902b774f146eb58bcb
laurenbriggs <laurenbriggs@dyn-118-139-45-185.its.monash.edu.au>
2017-05-23 10:32:46 +1000

    readd

---
+++ OPTION/assets/js/project3op2.js
@@ -0,0 +1,186 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+    $('.articlel').click(function() {
+        $('.articlel').toggleClass('showme');
+      });
+
+      $('.articler').click(function() {
+          $('.articler').toggleClass('showme');
+        });
+
+
+  $('.articlel').click(function() {
+      $('.centre').toggleClass('hide');
+    });
+
+    $('.articler').click(function() {
+        $('.centre').toggleClass('hide');
+      });
+
+
+
+      $('.facebook').mouseenter(function() {
+        $('.facebookimage').addClass('showme');
+      });
+
+      $('.facebook').mouseleave(function() {
+          $('.facebookimage').removeClass('showme');
+        });
+
+
+        $('.twitter').mouseenter(function() {
+          $('.twitterimage').addClass('showme');
+        });
+
+        $('.twitter').mouseleave(function() {
+            $('.twitterimage').removeClass('showme');
+          });
+
+
+    $('.iphone').mouseenter(function() {
+      $('.iphoneimage').addClass('showme');
+    });
+
+    $('.iphone').mouseleave(function() {
+        $('.iphoneimage').removeClass('showme');
+      });
+
+    $('.adam').mouseenter(function() {
+      $('.adamimage').addClass('showme');
+    });
+
+    $('.adam').mouseleave(function() {
+        $('.adamimage').removeClass('showme');
+      });
+
+
+    $('.homer').mouseenter(function() {
+      $('.homerimage').addClass('showme');
+    });
+
+    $('.homer').mouseleave(function() {
+        $('.homerimage').removeClass('showme');
+      });
+
+
+      $('.drunk').mouseenter(function() {
+        $('.drunkimage').addClass('showme');
+      });
+
+      $('.drunk').mouseleave(function() {
+          $('.drunkimage').removeClass('showme');
+        });
+
+    $('.cognitive').mouseenter(function() {
+      $('.cognitiveimage').addClass('showme');
+    });
+
+    $('.cognitive').mouseleave(function() {
+        $('.cognitiveimage').removeClass('showme');
+      });
+
+
+    $('.moses').mouseenter(function() {
+      $('.mosesimage').addClass('showme');
+    });
+
+    $('.moses').mouseleave(function() {
+        $('.mosesimage').removeClass('showme');
+      });
+
+
+    $('.ark').mouseenter(function() {
+      $('.arkimage').addClass('showme');
+    });
+
+    $('.ark').mouseleave(function() {
+        $('.arkimage').removeClass('showme');
+      });
+
+      $('.carson').mouseenter(function() {
+        $('.carsonimage').addClass('showme');
+      });
+
+      $('.carson').mouseleave(function() {
+          $('.carsonimage').removeClass('showme');
+        });
+
+
+        $('.font').mouseenter(function() {
+          $('.fontimage').addClass('showme');
+        });
+
+        $('.font').mouseleave(function() {
+            $('.fontimage').removeClass('showme');
+          });
+
+  $('.not').mouseenter(function() {
+    $('.notimage').addClass('showme');
+  });
+
+  $('.not').mouseleave(function() {
+      $('.notimage').removeClass('showme');
+    });
+
+  $('.ny').mouseenter(function() {
+    $('.nyimage').addClass('showme');
+  });
+
+  $('.ny').mouseleave(function() {
+      $('.nyimage').removeClass('showme');
+    });
+
+$('.column').mouseenter(function() {
+  $('.columnimage').addClass('showme');
+});
+
+$('.column').mouseleave(function() {
+    $('.columnimage').removeClass('showme');
+  });
+
+  $('.medium').mouseenter(function() {
+    $('.mediumimage').addClass('showme');
+  });
+
+  $('.medium').mouseleave(function() {
+      $('.mediumimage').removeClass('showme');
+    });
+
+    $('.forget').mouseenter(function() {
+      $('.forgetimage').addClass('showme');
+    });
+
+    $('.forget').mouseleave(function() {
+        $('.forgetimage').removeClass('showme');
+      });
+
+      $('.silicon').mouseenter(function() {
+        $('.siliconimage').addClass('showme');
+      });
+
+      $('.silicon').mouseleave(function() {
+          $('.siliconimage').removeClass('showme');
+        });
+
+    $('.user').mouseenter(function() {
+      $('.userimage').addClass('showme');
+    });
+
+    $('.user').mouseleave(function() {
+        $('.userimage').removeClass('showme');
+      });
+
+
+  $('.consume').mouseenter(function() {
+    $('.consumeimage').addClass('showme');
+  });
+
+  $('.consume').mouseleave(function() {
+      $('.consumeimage').removeClass('showme');
+    });
+
+  });
