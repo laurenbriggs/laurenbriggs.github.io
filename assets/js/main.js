762006497cc13ab1c61de1887e0eba21e387c7ed
laurenbriggs <laurenbriggs@MacBook-Pro-3.local>
2017-11-15 17:20:09 +1100

    update

---
+++ assets/js/main.js
@@ -0,0 +1,180 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+// NOTE: ngv
+
+$('.box_ngv').mouseenter(function() {
+  $('.box_ngv').addClass('showme');
+});
+
+$('.box_ngv').mouseleave(function() {
+    $('.box_ngv').removeClass('showme');
+  });
+
+  $('.box_ngv').mouseenter(function() {
+    $('.text_ngv').addClass('showme');
+  });
+
+  $('.box_ngv').mouseleave(function() {
+      $('.text_ngv').removeClass('showme');
+    });
+
+// NOTE: screen
+
+    $('.box_screen').mouseenter(function() {
+      $('.box_screen').addClass('showme');
+    });
+
+    $('.box_screen').mouseleave(function() {
+        $('.box_screen').removeClass('showme');
+      });
+
+      $('.box_screen').mouseenter(function() {
+        $('.text_screen').addClass('showme');
+      });
+
+      $('.box_screen').mouseleave(function() {
+          $('.text_screen').removeClass('showme');
+        });
+
+        // NOTE: hosp
+
+            $('.box_hosp').mouseenter(function() {
+              $('.box_hosp').addClass('showme');
+            });
+
+            $('.box_hosp').mouseleave(function() {
+                $('.box_hosp').removeClass('showme');
+              });
+
+              $('.box_hosp').mouseenter(function() {
+                $('.text_hosp').addClass('showme');
+              });
+
+              $('.box_hosp').mouseleave(function() {
+                  $('.text_hosp').removeClass('showme');
+                });
+
+
+
+        // NOTE: bon
+
+            $('.box_bon').mouseenter(function() {
+              $('.box_bon').addClass('showme');
+            });
+
+            $('.box_bon').mouseleave(function() {
+                $('.box_bon').removeClass('showme');
+              });
+
+              $('.box_bon').mouseenter(function() {
+                $('.text_bon').addClass('showme');
+              });
+
+              $('.box_bon').mouseleave(function() {
+                  $('.text_bon').removeClass('showme');
+                });
+// NOTE: pasta
+
+        $('.box_pasta').mouseenter(function() {
+          $('.box_pasta').addClass('showme');
+        });
+
+        $('.box_pasta').mouseleave(function() {
+            $('.box_pasta').removeClass('showme');
+          });
+
+          $('.box_pasta').mouseenter(function() {
+            $('.text_pasta').addClass('showme');
+          });
+
+          $('.box_pasta').mouseleave(function() {
+              $('.text_pasta').removeClass('showme');
+            });
+
+
+// NOTE: sounding
+
+    $('.box_sounding').mouseenter(function() {
+      $('.box_sounding').addClass('showme');
+    });
+
+    $('.box_sounding').mouseleave(function() {
+        $('.box_sounding').removeClass('showme');
+      });
+
+      $('.box_sounding').mouseenter(function() {
+        $('.text_sounding').addClass('showme');
+      });
+
+      $('.box_sounding').mouseleave(function() {
+          $('.text_sounding').removeClass('showme');
+        });
+
+
+        // NOTE: wallpaper
+
+            $('.box_wallpaper').mouseenter(function() {
+              $('.box_wallpaper').addClass('showme');
+            });
+
+            $('.box_wallpaper').mouseleave(function() {
+                $('.box_wallpaper').removeClass('showme');
+              });
+
+              $('.box_wallpaper').mouseenter(function() {
+                $('.text_wallpaper').addClass('showme');
+              });
+
+              $('.box_wallpaper').mouseleave(function() {
+                  $('.text_wallpaper').removeClass('showme');
+                });
+
+        // NOTE: sense
+
+            $('.box_sense').mouseenter(function() {
+              $('.box_sense').addClass('showme');
+            });
+
+            $('.box_sense').mouseleave(function() {
+                $('.box_sense').removeClass('showme');
+              });
+
+              $('.box_sense').mouseenter(function() {
+                $('.text_sense').addClass('showme');
+              });
+
+              $('.box_sense').mouseleave(function() {
+                  $('.text_sense').removeClass('showme');
+                });
+
+
+
+
+    $('.toggle').mouseenter(function() {
+      $('.nav').addClass('showme');
+    });
+
+    $('.toggle').click(function() {
+        $('.nav').removeClass('showme');
+      });
+
+      $('.toggle').mouseenter(function() {
+        $('.toggle').addClass('spin');
+      });
+
+      $('.toggle').click(function() {
+          $('.toggle').removeClass('spin');
+        });
+
+
+
+
+
+
+
+
+
+  });
